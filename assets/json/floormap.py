fb0d28e6395a15a82e20ba62ee152f90ebf74939
Astalsi401 <Astalsi401@users.noreply.github.com>
2024-02-19 03:10:09 +0000

    deploy: 51645b97ce0bec7285ebbb9474f698dfa2ed067b

--- assets/json/floormap.py
+++ assets/json/floormap.py
@@ -13,7 +13,7 @@ def getEvent(data: dict) -> dict:
 
 
 def getDefault(data: dict) -> dict:
-    return {key: data[key] for key in data.keys() if key != 'event'}
+    return {key: data[key] for key in data.keys() if key not in ['event', 'tag']}
 
 
 def getBoothPos(data: dict) -> dict:
@@ -31,9 +31,10 @@ with open('floormap.json', 'r', encoding='utf-8') as f:
     events = [getEvent(data) for data in floormap if 'event' in data.keys() and 'booth' == data['type']]
     default = [getDefault(data) for data in floormap if data['type'] in ['icon', 'wall', 'room', 'pillar']]
     defaultTexts = [data for data in floormap if data['type'] == 'text' and data['text']['tc'][0] in ['I', 'J', 'K', 'L', 'M', 'N']]
+    booth = [data for data in floormap if data['type'] not in ['icon', 'wall', 'room', 'pillar'] or (data['type'] == 'text' and data['text']['tc'][0] not in ['I', 'J', 'K', 'L', 'M', 'N'])]
     minify = (',', ':')
     open('2023/events.json', 'w').write(dumps(events, ensure_ascii=False, separators=minify))
     open('2023/boothInfo.json', 'w').write(dumps([getBoothInfo(data) for data in floormap if 'booth' == data['type']], ensure_ascii=False, separators=minify))
     open('elems.json', 'w').write(dumps(default+defaultTexts, ensure_ascii=False, separators=minify))
     open('boothPos.json', 'w').write(dumps([getBoothPos(data) for data in floormap if 'booth' == data['type']], ensure_ascii=False, separators=minify))
-    print(types)
+    open('2023/booth.json', 'w').write(dumps(booth, ensure_ascii=False, separators=minify))
