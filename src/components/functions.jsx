73f3f8fe2e3a1027f96e1110e2996962eb39e02e
Astalsi401 <misting401@gmail.com>
2024-08-28 10:20:35 +0800

    update: replace 'px' to ''

--- src/components/functions.jsx
+++ src/components/functions.jsx
@@ -30,26 +30,26 @@ export const getMapElems = async ({ params: { year, category } }) => {
 };
 
 export const zoomCalculator = (clientX, clientY, graph, svg, r, rMax = 10) => {
-  const box = graph.getBoundingClientRect();
-  const prevScale = parseFloat(svg.style.scale || 1);
+  const box = graph.getBoundingClientRect(),
+    prevScale = parseFloat(svg.style.scale || 1);
   let [prevx, prevy] = svg.style.translate.replace("px", "").split(" ");
   prevx = parseFloat(prevx || 0);
   prevy = parseFloat(prevy || 0);
   let scale = prevScale * r;
   scale = scale < 0.9 ? 0.9 : scale > rMax ? rMax : scale;
-  let w = svg.clientWidth * prevScale;
-  let h = svg.clientHeight * prevScale;
-  let x = (graph.clientWidth - w) / 2 + prevx;
-  let y = (graph.clientHeight - h) / 2 + prevy;
-  let originX = clientX - box.x - x - w / 2;
-  let originY = clientY - box.y - y - h / 2;
-  let xNew = originX - (originX / prevScale) * scale + prevx;
-  let yNew = originY - (originY / prevScale) * scale + prevy;
+  let w = svg.clientWidth * prevScale,
+    h = svg.clientHeight * prevScale,
+    x = (graph.clientWidth - w) / 2 + prevx,
+    y = (graph.clientHeight - h) / 2 + prevy,
+    originX = clientX - box.x - x - w / 2,
+    originY = clientY - box.y - y - h / 2,
+    xNew = originX - (originX / prevScale) * scale + prevx,
+    yNew = originY - (originY / prevScale) * scale + prevy;
   svg.style.scale = scale;
   svg.style.translate = `${xNew}px ${yNew}px`;
 };
 export const dragCalculator = (x, y, svg) => {
-  const [prevx, prevy] = svg.style.translate.split(" ");
+  const [prevx, prevy] = svg.style.translate.replace("px", "").split(" ");
   svg.style.translate = `${parseFloat(prevx || 0) + x}px ${parseFloat(prevy || 0) + y}px`;
 };
 export const resetViewbox = (svg) => {
