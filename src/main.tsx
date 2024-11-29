068ce8f0aaf32df508aed45bd24cdc33744fd11e
Astalsi401 <misting401@gmail.com>
2024-11-29 15:57:29 +0800

    update: routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,39 +7,35 @@ import { getMapElems } from "@functions";
 import { App, ErrorPage } from "@routes";
 import "@styles/floormap-main.scss";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/floormap/",
-      element: (
-        <>
-          <div>
-            <Link to={`/floormap/2023/booths`}>2023 Booths</Link>
-          </div>
-          <div>
-            <Link to={`/floormap/2024/areas`}>2024 Area</Link>
-          </div>
-          <div>
-            <Link to={`/floormap/2024/booths`}>2024 booths</Link>
-          </div>
-          <div>
-            <Link to={`/floormap/2024/booths?edit=1`}>2024 Booths edit</Link>
-          </div>
-        </>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/floormap/:year/:category",
-      element: <App />,
-      errorElement: <ErrorPage />,
-      loader: getMapElems,
-    },
-  ],
+const routes = [
   {
-    future: { v7_relativeSplatPath: true, v7_fetcherPersist: true, v7_normalizeFormMethod: true, v7_partialHydration: true, v7_skipActionErrorRevalidation: true },
-  }
-);
+    path: "/floormap/",
+    element: (
+      <>
+        <div>
+          <Link to={`/floormap/2023/booths`}>2023 Booths</Link>
+        </div>
+        <div>
+          <Link to={`/floormap/2024/areas`}>2024 Area</Link>
+        </div>
+        <div>
+          <Link to={`/floormap/2024/booths`}>2024 booths</Link>
+        </div>
+        <div>
+          <Link to={`/floormap/2024/booths?edit=1`}>2024 Booths edit</Link>
+        </div>
+      </>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/floormap/:year/:category",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    loader: getMapElems,
+  },
+];
+const router = createBrowserRouter(routes, { future: { v7_relativeSplatPath: true, v7_fetcherPersist: true, v7_normalizeFormMethod: true, v7_partialHydration: true, v7_skipActionErrorRevalidation: true } });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
